221581d02ea9a5128af8078cd3aeffe06259c59d
Trevor Richards <trev@trevdev.ca>
2025-03-13 16:27:48 -0700

    feat: helper functions for parsing and searching

--- src/soap_sync/xml_soap.clj
+++ src/soap_sync/xml_soap.clj
@@ -1,10 +1,15 @@
 (ns soap-sync.xml-soap
   (:require [clj-http.client :as client]
             [clojure.java.io :as io]
-            [clojure.data.xml :refer [element emit-str]]
+            [clojure.data.xml :refer [element emit-str parse-str]]
             [soap-sync.csv :as csv]))
 
 (defn to-string [xml-object] (emit-str xml-object))
+(defn to-xml-map [xml-string] (parse-str xml-string))
+
+(defn get-elements-by-tag-name [tag-name xml-map]
+  (filter #(= (:tag %) tag-name)
+          (tree-seq #(seq? (:content %)) #(:content %) xml-map)))
 
 (defn create-account-detail-item [guid]
   (element :AccountDetailListRequestItem {}
@@ -24,7 +29,7 @@
                                (map create-account-detail-item guids))))))
 
 (defn create-soap-body [body-content]
-     (element :soapenv:Body {} body-content))
+  (element :soapenv:Body {} body-content))
 
 (defn create-soap-envelope [body]
   (element :soapenv:Envelope {:xmlns:soapenv
