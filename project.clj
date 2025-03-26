2796cd1b2c17083023c0826af3a4c7a844e62695
Trevor Richards <trev@trevdev.ca>
2025-03-26 13:31:24 -0700

    feat: project description and version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject bceid-soap-sync "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject bceid-soap-sync "0.1.0"
+  :description "A one-off tool for retrieving BCeID information."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
