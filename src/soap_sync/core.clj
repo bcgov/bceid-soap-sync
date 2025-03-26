9acbb97fa45bc77a936d2fefb1476027a57961a9
Trevor Richards <trev@trevdev.ca>
2025-03-26 10:50:07 -0700

    feat: main implementation

--- src/soap_sync/core.clj
+++ src/soap_sync/core.clj
@@ -18,4 +18,60 @@
                   :content-type "text/xml;charset=utf-8"
                   :throw-exceptions false})))
 
-(defn -main [])
+(defn extract-csv-row [detail-list-item]
+  (let [individual (first (xs/get-elements-by-tag-name
+                           :individualIdentity detail-list-item))
+        business (first (xs/get-elements-by-tag-name
+                         :business detail-list-item))]
+    [(->> detail-list-item
+          (xs/get-element-by-tag-name :userId)
+          xs/get-tag-value)
+     (->> detail-list-item
+          (xs/get-element-by-tag-name :guid)
+          xs/get-tag-value)
+     (->> individual
+          (xs/get-element-by-tag-name :firstname)
+          xs/get-tag-value)
+     (->> individual
+          (xs/get-element-by-tag-name :surname)
+          xs/get-tag-value)
+     (->> business
+          (xs/get-element-by-tag-name :legalName)
+          xs/get-tag-value)
+     (->> business
+          (xs/get-element-by-tag-name :businessNumber)
+          xs/get-tag-value)
+     (->> business
+          (xs/get-element-by-tag-name :businessNumberVerifiedFlag)
+          xs/get-tag-value)
+     (->> business
+          (xs/get-element-by-tag-name :statementOfRegistrationNumber)
+          xs/get-tag-value)
+     (->> business
+          (xs/get-element-by-tag-name :incorporationNumber)
+          xs/get-tag-value)
+     (->> detail-list-item (xs/get-element-by-tag-name :failureCode)
+          :content first)]))
+
+(defn generate-output-data [response-body]
+  (into [] (concat
+            [["User ID" "GUID" "First Name" "Last Name" "Legal Business Name"
+              "Business Number" "Business Number Verified"
+              "Statement of Registration Number" "Incorporation Number"
+              "Failure Code"]]
+            (map extract-csv-row
+                 (xs/get-elements-by-tag-name :AccountDetailListResponseItem
+                                              (xs/to-xml-map response-body))))))
+
+(defn -main []
+  (let [api-response (send-soap-request "getAccountDetailList"
+                                        (-> (map #(list (get % "GUID")
+                                                        (get % "BCeID"))
+                                                 (csv/get-input-data))
+                                            xs/create-account-detail-list
+                                            xs/create-soap-body
+                                            xs/create-soap-envelope
+                                            xs/to-string))]
+    (when (not (= 200 (:status api-response)))
+      (throw (Throwable. "API response was not OK.")))
+    (csv/write-output-data (generate-output-data (:body api-response)))))
