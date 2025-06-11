158933194f78b74a2f24a333c9104b19fca39231
Trevor Richards <trev@trevdev.ca>
2025-06-11 09:52:18 -0700

    fix: LSP linter suggestions

--- src/soap_sync/xml_soap.clj
+++ src/soap_sync/xml_soap.clj
@@ -1,31 +1,31 @@
 (ns soap-sync.xml-soap
-  (:require [clj-http.client :as client]
-            [clojure.java.io :as io]
-            [clojure.data.xml :refer [element emit-str parse-str]]
-            [soap-sync.csv :as csv]))
+  (:require [clojure.data.xml :refer [element emit-str parse-str]]))
 
 (defn to-string [xml-object] (emit-str xml-object))
 (defn to-xml-map [xml-string] (parse-str xml-string))
 
-(defn get-elements-by-tag-name [tag-name xml-map]
+(defn get-elements-by-tag-name
   "Get all elements by tag name."
+  [tag-name xml-map]
   (filter #(= (:tag %) tag-name)
           (tree-seq #(seq? (:content %)) #(:content %) xml-map)))
 
-(defn get-element-by-tag-name [tag-name xml-map]
+(defn get-element-by-tag-name
   "Get the first element by tag name."
+  [tag-name xml-map]
   (first (get-elements-by-tag-name tag-name xml-map)))
 
-(defn get-tag-value [xml-tag]
+(defn get-tag-value
   "Many objects have a nested 'value' tag that actually holds the tag value.
-This makes extracting that nested value feel less repetative."
+  This makes extracting that nested value feel less repetative."
+  [xml-tag]
   (->> xml-tag :content first :content first))
 
 (defn create-account-detail-item [ids]
   (let [[guid user-id] ids]
     (element :AccountDetailListRequestItem {}
-             (cond (not (empty? guid)) (element :userGuid {} guid)
-                   (not (empty? user-id)) (element :userId {} user-id)
+             (cond (seq guid) (element :userGuid {} guid)
+                   (seq user-id) (element :userId {} user-id)
                    :else (throw (Throwable. "GUID or userId must be supplied")))
              (element :accountTypeCode {} "Business"))))
 
