514302314006bd898bb69140f11beeddd42d68d5
Trevor Richards <trev@trevdev.ca>
2025-06-17 11:39:35 -0700

    feat: chunk requests, retain original ids

--- src/soap_sync/core.clj
+++ src/soap_sync/core.clj
@@ -1,7 +1,9 @@
 (ns soap-sync.core
   (:gen-class)
-  (:require [soap-sync.csv :as csv]
+  (:require [clojure.core.async :refer [chan thread <!! >!! close!]]
+            [soap-sync.csv :as csv]
             [soap-sync.xml-soap :as xs]
+            [soap-sync.utils :refer [chunk-rows]]
             [clj-http.client :as client]))
 
 (defn send-soap-request [action soap-envelope]
@@ -18,11 +20,9 @@
                   :content-type "text/xml;charset=utf-8"
                   :throw-exceptions false})))
 
-(defn extract-csv-row [detail-list-item]
+(defn extract-csv-row [[detail-list-item [guid bceid]]]
   (let [individual (first (xs/get-elements-by-tag-name
-                           :individualIdentity detail-list-item))
-        business (first (xs/get-elements-by-tag-name
-                         :business detail-list-item))]
+                            :individualIdentity detail-list-item))]
     [(->> detail-list-item
           (xs/get-element-by-tag-name :userId)
           xs/get-tag-value)
@@ -35,43 +35,56 @@
      (->> individual
           (xs/get-element-by-tag-name :surname)
           xs/get-tag-value)
-     (->> business
-          (xs/get-element-by-tag-name :legalName)
-          xs/get-tag-value)
-     (->> business
-          (xs/get-element-by-tag-name :businessNumber)
-          xs/get-tag-value)
-     (->> business
-          (xs/get-element-by-tag-name :businessNumberVerifiedFlag)
-          xs/get-tag-value)
-     (->> business
-          (xs/get-element-by-tag-name :statementOfRegistrationNumber)
-          xs/get-tag-value)
-     (->> business
-          (xs/get-element-by-tag-name :incorporationNumber)
-          xs/get-tag-value)
      (->> detail-list-item (xs/get-element-by-tag-name :failureCode)
-          :content first)]))
+          :content first)
+     bceid
+     guid]))
+
+(defn prepare-callout [chunk]
+  (let [ids (map #(list (get % "GUID")
+                        (get % "BCeID")) chunk)]
+    (-> ids
+        xs/create-account-detail-list
+        xs/create-soap-body
+        xs/create-soap-envelope
+        xs/to-string
+        (vector ids))))
+
+(prepare-callout (csv/get-input-data))
+
+(defn process-api-callouts [callouts]
+  (let [channel (chan)]
+    (thread (doseq [[envelope ids] callouts]
+              (>!! channel (vector (send-soap-request "getAccountDetailList" envelope)
+                                   ids)))
+            (close! channel))
+    (loop [responses []
+           callout 0]
+      (if-let [[res ids] (<!! channel)]
+        (do
+          (println "Received response from callout " callout)
+          (println "API Response Code: " (:status res))
+          (recur (if-not (= 200 (:status res))
+                   responses
+                   (conj responses (vector (:body res) ids)))
+                 (inc callout)))
+        responses))))
+
+(defn get-detail-list-presponse-item [[response, ids]]
+  (map vector
+       (xs/get-elements-by-tag-name :AccountDetailListResponseItem
+                                    (xs/to-xml-map response))
+       ids))
 
 (defn generate-output-data [response-body]
   (into [] (concat
-            [["User ID" "GUID" "First Name" "Last Name" "Legal Business Name"
-              "Business Number" "Business Number Verified"
-              "Statement of Registration Number" "Incorporation Number"
-              "Failure Code"]]
-            (map extract-csv-row
-                 (xs/get-elements-by-tag-name :AccountDetailListResponseItem
-                                              (xs/to-xml-map response-body))))))
+             [["User ID" "GUID" "First Name" "Last Name" "Failure Code"
+               "Source BCeID" "Source GUID"]]
+             (map extract-csv-row response-body))))
 
 (defn -main []
-  (let [api-response (send-soap-request "getAccountDetailList"
-                                        (-> (map #(list (get % "GUID")
-                                                        (get % "BCeID"))
-                                                 (csv/get-input-data))
-                                            xs/create-account-detail-list
-                                            xs/create-soap-body
-                                            xs/create-soap-envelope
-                                            xs/to-string))]
-    (when (not (= 200 (:status api-response)))
-      (throw (Throwable. "API response was not OK.")))
-    (csv/write-output-data (generate-output-data (:body api-response)))))
+  (let [api-responses (->> (chunk-rows 100 (csv/get-input-data))
+                           (map prepare-callout)
+                           process-api-callouts
+                           (mapcat get-detail-list-presponse-item))]
+    (csv/write-output-data (generate-output-data api-responses))))
